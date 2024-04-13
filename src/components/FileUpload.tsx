e4a6c0b15001a8ea4e2ab48a80b2e9a21b7ec06c
Mantavya Soni <mds33@njit.edu>
2024-04-13 19:50:15 -0400

    Initial Commit

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,82 @@
+'use client'
+import { uploadToS3 } from '@/lib/s3'
+import { useMutation } from '@tanstack/react-query'
+import { Inbox, Loader2 } from 'lucide-react'
+import React from 'react'
+import { useDropzone } from 'react-dropzone'
+import axios from 'axios'
+import {toast} from 'react-hot-toast'
+import {useRouter} from 'next/navigation'
+
+const FileUpload = () => {
+    const router = useRouter()
+    const [uploading, setUploading] = React.useState(false);
+    
+    const { mutate } = useMutation({
+        mutationFn: async ({file_key, file_name}: {file_key: string, file_name: string}) => {
+            // You no longer need to set uploading here since it's handled in onDrop
+            const response = await axios.post('/api/create-chat', {file_key, file_name});
+            return response.data;
+        },
+        // Optional: You can handle onSuccess and onError here if specific actions are needed
+    });
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: {'application/pdf': ['.pdf']},
+        maxFiles: 1,
+        onDrop: async (acceptedFiles) => {
+            const file = acceptedFiles[0];
+            if (file.size > 10 * 1024 * 1024) {
+                toast.error('Please upload a smaller file');
+                return;
+            }
+
+            try {
+                setUploading(true); // Start uploading
+                const data = await uploadToS3(file);
+                console.log('meow', data)
+                if (!data?.file_key || !data.file_name) {
+                    toast.error('Something went wrong');
+                    return;
+                }
+                mutate(data, {
+                    onSuccess: ({chat_id}) => {
+                        alert("Chat Created");
+                        router.push(`/chat/${chat_id}`)
+                        // toast.success('Chat Created')
+                    },
+                    onError: (err) => {
+                        console.log(err);
+                    },
+                });
+            } catch (error) {
+                toast.error('An unexpected error occurred');
+            } finally {
+                setUploading(false); // End uploading
+            }
+        },
+    });
+
+    return (
+        <div className='p-2 bg-white rounded-xl'>
+            <div {...getRootProps({
+                className: "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
+            })}>
+                <input {...getInputProps()} />
+                {uploading ? ( // Use the uploading state to determine if loading content should be displayed
+                    <>
+                        <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+                        <p className="mt-2 text-sm text-slate-400">Spilling Tea to GPT...</p>
+                    </>
+                ) : (
+                    <>
+                        <Inbox className="w-10 h-10 text-blue-500" />
+                        <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default FileUpload;
