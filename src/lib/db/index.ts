ce631fb30a7e4ddb8857c7e179aa7b377ec34805
Mantavya Soni <mds33@njit.edu>
2024-04-13 20:52:01 -0400

    Another modification

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,5 @@
-import {neon, neonConfig} from '@neondatabase/serverless';
-import {drizzle} from 'drizzle-orm/neon-http';
+import {NeonQueryFunction, neon, neonConfig} from '@neondatabase/serverless';
+import {drizzle} from 'drizzle-orm/neon-http'
 
 neonConfig.fetchConnectionCache=true;
 
@@ -7,6 +7,9 @@ if (!process.env.DATABASE_URL) {
     throw new Error('database URl not found');
 }
 
-const sql = neon(process.env.DATABASE_URL);
+// const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL) as unknown as NeonQueryFunction<boolean, boolean>;
+
 
 export const db = drizzle(sql);
+
